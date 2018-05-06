513a52dbeca8cf90f83391c621f60b1f98e226bc
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2018-05-06 21:58:01 +0200

    Replace google maps with image

--- www/js/wr.js
+++ www/js/wr.js
@@ -120,54 +120,6 @@
 
 
 
-    // Do map
-
-    var map,
-        locations = {
-            venue : { title : 'Conference Venue', lat : 59.93604, lng : 10.76576 }
-        };
-
-    function hideMapLink() {
-        var maplink = document.getElementsByClassName("maplink");
-        if (maplink && maplink.length) maplink[0].style.display = "none";
-    }
-
-    function showMap() {
-        var options = {
-            zoom: 15,
-            center: new google.maps.LatLng( locations.venue.lat, locations.venue.lng ),
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            scrollwheel: false
-        };
-        map = new google.maps.Map(document.getElementById('map'), options );
-
-        var marker = new google.maps.Marker({
-            position: map.getCenter(),
-            map: map,
-            title: locations.venue.title
-        });
-
-        google.maps.event.addDomListener( window, 'resize', function() {
-            map.setCenter( map.getCenter() );
-        });
-
-        hideMapLink();
-    }
-
-    // showMap needs to be global for async map loading
-    window["showMap"] = showMap;
-
-    function loadMap() {
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://maps.googleapis.com/maps/api/js?sensor=false&callback=showMap";
-        document.body.appendChild(script);
-    }
-
-    if (!youCanTouchThis) {
-        loadMap();
-    }
-
 
 
     // Flip schedule
